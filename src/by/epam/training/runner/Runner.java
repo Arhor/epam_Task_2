c4dbcb5327aaa0d7f9f20c07ab4379e11b304b7c
Max.Burishinets <arhor18@gmail.com>
2018-07-25 12:06:26 +0300

    changed layout pattern, added fisher

--- src/by/epam/training/runner/Runner.java
+++ src/by/epam/training/runner/Runner.java
@@ -15,7 +15,15 @@ public class Runner {
          * TODO: whole project -_-
          */
         IComposite wholeText = null;
-        wholeText = TextParser.parseToSentences("Hello world... My name is Max! How's your doing!? ");
+        wholeText = TextParser.parseToSentences("Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit, sed do eiusmod tempor incidid" +
+                "unt ut labore et dolore magna aliqua. Ut enim ad minim veni" +
+                "am, quis nostrud exercitation ullamco laboris nisi ut aliqu" +
+                "ip ex ea commodo consequat. Duis aute irure dolor in repreh" +
+                "enderit in voluptate velit esse cillum dolore eu fugiat nul" +
+                "la pariatur. Excepteur sint occaecat cupidatat non proident" +
+                ", sunt in culpa qui officia deserunt mollit anim id est lab" +
+                "orum.");
 
         for (int i = 0; i < ((CompositeObject)wholeText).size(); i++) {
             LOG.info(i + 1 + " sentence is: " + wholeText.get(i));
