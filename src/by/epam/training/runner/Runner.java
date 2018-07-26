aa02f8b29f139772b187edb09ca4d8abdb8d3685
Max.Burishinets <arhor18@gmail.com>
2018-07-26 10:48:30 +0300

    file reader added

--- src/by/epam/training/runner/Runner.java
+++ src/by/epam/training/runner/Runner.java
@@ -5,6 +5,10 @@ import by.epam.training.model.*;
 import by.epam.training.service.TextParser;
 import org.apache.log4j.*;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Runner {
 
     private static final Logger LOG = Logger.getLogger(Runner.class);
@@ -14,16 +18,23 @@ public class Runner {
          * Just for testing IComposite-pattern !!!
          * TODO: whole project -_-
          */
+        StringBuilder sb = new StringBuilder();
+        File file = new File("text.txt");
+        try (FileReader fileReader = new FileReader(file)) {
+            int b;
+            do {
+                b = fileReader.read();
+                if (b != -1) {
+                    sb.append((char)b);
+                }
+            } while (b != -1);
+        } catch (IOException e) {
+            LOG.error("I/O exception", e);
+        }
+
+
         IComposite wholeText = null;
-        wholeText = TextParser.parseToSentences("Lorem ipsum dolor sit amet," +
-                " consectetur adipiscing elit, sed do eiusmod tempor incidid" +
-                "unt ut labore et dolore magna aliqua. Ut enim ad minim veni" +
-                "am, quis nostrud exercitation ullamco laboris nisi ut aliqu" +
-                "ip ex ea commodo consequat. Duis aute irure dolor in repreh" +
-                "enderit in voluptate velit esse cillum dolore eu fugiat nul" +
-                "la pariatur. Excepteur sint occaecat cupidatat non proident" +
-                ", sunt in culpa qui officia deserunt mollit anim id est lab" +
-                "orum.");
+        wholeText = TextParser.parseToSentences(sb.toString());
 
         for (int i = 0; i < ((CompositeObject)wholeText).size(); i++) {
             LOG.info(i + 1 + " sentence is: " + wholeText.get(i));
